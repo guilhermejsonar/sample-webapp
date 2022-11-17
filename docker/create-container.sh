37fd59b24e1818fa5eead2a58df29f311869f73a
guilherme.facanha <guilherme.facanha@imperva.com>
2022-11-17 15:51:00 -0800

    [docker] add OS libs

--- docker/create-container.sh
+++ docker/create-container.sh
@@ -5,3 +5,6 @@ sudo docker build -t employee-crud-img .
 
 echo 'Create container...' 
 sudo docker run --privileged -it -d -p 8000:8000 --name employee-crud localhost/employee-crud-img
+
+# deploy external lib
+mvn deploy:deploy-file -DgroupId=com.revenera -DartifactId=tra-run -Dversion=1.0 -Dpackaging=jar -Dfile=tra-run.jar -DrepositoryId=releases -Durl=https://artifactory.imperva-cc395.com/artifactory/libs-release 
